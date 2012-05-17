5a9e9009254e2e688f663ee5d4b91f7dd2f3fd4e
Bryan <bryan@bryan-VirtualBox.(none)>
2012-05-16 22:51:32 -0400

    Misc Fixes

--- arch/arm/mach-omap2/omap4_trim_quirks.c
+++ arch/arm/mach-omap2/omap4_trim_quirks.c
@@ -20,7 +20,6 @@
 #define OMAP4_DPLL_MPU_TRIMMED_VAL_2P4	(0x1 << 18)
 #define OMAP4_DPLL_MPU_TRIMMED_VAL_3P0	(0x3 << 18)
 #define OMAP4_DPLL_MPU_TRIMMED_MASK	(BIT(19) | BIT(18))
-
 /*
  * Trim value has to be written to CONTROL_EFUSE_2 according to
  * OMAP4430 errata i684 (version B)
@@ -29,8 +28,9 @@
 #define OMAP4_LPDDR2_I684_FIX_VALUE	0x004E4000
 #define OMAP4_PROD_ID_I684_MASK		0x000C0000
 
+
 static bool bgap_trim_sw_overide;
-static bool dpll_trim_override;
+static bool dpll_trim_override = true;
 static bool ddr_io_trim_override;
 
 /**
@@ -76,7 +76,7 @@ int omap4_ldo_trim_configure(void)
 
 	/* Required for DPLL_MPU to lock at 2.4 GHz */
 	if (dpll_trim_override)
-		omap_ctrl_writel(0x29, OMAP4_CTRL_MODULE_CORE_DPLL_NWELL_TRIM_0);
+		omap_ctrl_writel(0x2b, OMAP4_CTRL_MODULE_CORE_DPLL_NWELL_TRIM_0);
 
 	return 0;
 }
@@ -93,9 +93,11 @@ static __init void omap4460_mpu_dpll_trim_override(void)
 	switch (val) {
 	case OMAP4_DPLL_MPU_TRIMMED_VAL_3P0:
 		/* all ok.. */
+		pr_info("[imoseyon] trim_val 3P0\n");
 		break;
 	case OMAP4_DPLL_MPU_TRIMMED_VAL_2P4:
 		/* Cross check! */
+		pr_info("[imoseyon] trim_val 2P4 (1.5Ghz capable, hw trimmed)\n");
 		if (omap4_has_mpu_1_5ghz()) {
 			WARN(1, "%s: OMAP is 1.5GHz capable, trimmed=1.2GHz!\n",
 				__func__);
@@ -106,6 +108,7 @@ static __init void omap4460_mpu_dpll_trim_override(void)
 			__func__, val);
 		/* fall through and use override */
 	case 0:
+		pr_info("[imoseyon] trim_val unknown (1.5Ghz capable, sw trimmed)\n");
 		/*
 		 * For PRE_RTP devices: Not trimmed, use s/w override!
 		 * We only support unto 1.2GHz with s/w override,
@@ -163,7 +166,7 @@ static __init int omap4_ldo_trim_init(void)
 	 * 0  1  Fixed test program issue of overlapping of LPDDR & SmartIO
 	 *	 efuse fields, SW WA needed for LPDDR.
 	 * 1  1  New LPDDR trim formula to compensate for vertical vs horizontal
-	 *	 cell layout.  No overwrite required.
+	 *	 cell layout. No overwrite required.
 	 */
 	if (cpu_is_omap443x()) {
 		u32 prod_id;
@@ -178,3 +181,4 @@ static __init int omap4_ldo_trim_init(void)
 	return omap4_ldo_trim_configure();
 }
 arch_initcall(omap4_ldo_trim_init);
+
