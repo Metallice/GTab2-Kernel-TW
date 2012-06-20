50c5bf19d693caba8dde283545db665011a5bab0
Metallice <BryanUofD@gmail.com>
2012-06-20 14:43:42 -0400

    Revert "gpu: pvr: Update to DDK 1.8@873556"
    This reverts commit 557ef159fc0174eac86b2319e1a7fddef275ca96.

--- drivers/gpu/pvr/sgx/sgxinfokm.h
+++ drivers/gpu/pvr/sgx/sgxinfokm.h
@@ -128,7 +128,7 @@ typedef struct _PVRSRV_SGXDEV_INFO_
 #if defined(FIX_HW_BRN_29823)
 	PPVRSRV_KERNEL_MEM_INFO	psKernelDummyTermStreamMemInfo; 
 #endif
-#if defined(SGX_FEATURE_VDM_CONTEXT_SWITCH) && defined(FIX_HW_BRN_31559)
+#if defined(SGX_FEATURE_VDM_CONTEXT_SWITCH) && defined(FIX_HW_BRN_31425)
 	PPVRSRV_KERNEL_MEM_INFO	psKernelVDMSnapShotBufferMemInfo; 
 	PPVRSRV_KERNEL_MEM_INFO	psKernelVDMCtrlStreamBufferMemInfo; 
 #endif
@@ -441,7 +441,7 @@ typedef struct _SGX_CCB_KICK_KM_
 #else
 	
 	IMG_UINT32	ui32NumSrcSyncs;
-	IMG_HANDLE	ahSrcKernelSyncInfo[SGX_MAX_SRC_SYNCS_TA];
+	IMG_HANDLE	ahSrcKernelSyncInfo[SGX_MAX_SRC_SYNCS];
 #endif
 
 	
